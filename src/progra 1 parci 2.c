0012dc60ddb7cf05bdedae641e76506c84645e01
jesusochoa-ux <jeanpau988@gmail.com>
2021-09-25 17:48:32 -0300

    first commit

---
+++ src/progra 1 parci 2.c
@@ -0,0 +1,45 @@
+/*
+ ============================================================================
+ Name        : progra.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+void funcion(int*, int);
+int main()
+{
+	int num1=65;
+	int num2=50;
+
+	funcion(&num1, num2);
+	printf("%d %d", num1, num2);
+	return 0;
+}
+void funcion(int* a, int b)
+{
+	*a += 43;
+	b += 18;
+}
+*/
+/*
+int main()
+{
+int numero;
+int* punteroA;
+int* punteroB;
+numero=45;
+
+*punteroA = numero*2;
+punteroB = punteroA;
+*punteroB = *punteroA + 5;
+
+printf("el valor del numero es: %d", numero);
+}
+*/
